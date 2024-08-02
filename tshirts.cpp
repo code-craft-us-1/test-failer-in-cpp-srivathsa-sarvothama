bbd9520df2a89801d1b7b18348d76ba3391c5b68
Sudeep Prasad <sudeep.rp@gmail.com>
2024-08-02 13:25:09 +0530

    lint

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include <assert.h>
+#include <iostream>
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if (cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if (cms > 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if (cms > 42) {
         sizeName = 'L';
     }
     return sizeName;
